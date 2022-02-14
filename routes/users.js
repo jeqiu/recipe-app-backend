6594fdea51f9a3c75a7f9e6316d7acdaffc8eae5
jeqiu <74211080+jeqiu@users.noreply.github.com>
2022-02-13 16:20:06 -0800

    use Postman to test query

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const usersRouter = require('express').Router();
+const { pool } = require('../config');
 
 // GET  /users | getUsers()
 // GET  /users/:id | getUserById()
@@ -6,10 +7,16 @@ const usersRouter = require('express').Router();
 // PUT  /users/:id | updateUser()
 // DELETE  /users/:id | deleteUser()
 
-usersRouter.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id]);
-  res.send(rows[0]);
+usersRouter.get('/:username', async (req, res) => {
+  const { username } = req.params;
+  console.log(`username is: ${username}`);
+  const results = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+  console.log(results.rows);
+  if (results.rows[0]) {
+    res.status(200).json(results.rows[0]);
+  } else {
+    res.status(404).json('no such username');
+  }
 });
 
 module.exports = usersRouter;
